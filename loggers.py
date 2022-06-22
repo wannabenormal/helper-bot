7f8b26c34fe48af15fbb1409e9222c305edd4320
Alexey Pesotsky <pesthedog@mail.ru>
2022-06-22 21:17:58 +0400

    Add logger to vk_bot

--- loggers.py
+++ loggers.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 
 class TgLogsHandler(logging.Handler):
@@ -10,3 +11,18 @@ class TgLogsHandler(logging.Handler):
     def emit(self, record):
         log_entry = self.format(record)
         self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+class VkLogsHandler(logging.Handler):
+    def __init__(self, vk_api, user_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.vk_api = vk_api
+        self.user_id = user_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.vk_api.messages.send(
+            user_id=self.user_id,
+            message=log_entry,
+            random_id=random.randint(1, 1000)
+        )
--- vk_bot.py
+++ vk_bot.py
@@ -1,45 +1,59 @@
 import random
+import logging
 
 from environs import Env
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
-from google.cloud import dialogflow
+
+from loggers import VkLogsHandler
+from api_google import get_response_to_message
+
+
+logger = logging.getLogger('VkLogsHandler')
 
 env = Env()
 env.read_env()
 
 
-def message_handler(event, vk_api, google_project_id, language_code='ru_RU'):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(
-        google_project_id,
-        event.user_id
-    )
-
-    text_input = dialogflow.TextInput(
-        text=event.text,
-        language_code=language_code
-    )
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    if not response.query_result.intent.is_fallback:
-        vk_api.messages.send(
-            user_id=event.user_id,
-            message=response.query_result.fulfillment_text,
-            random_id=random.randint(1, 1000)
+def message_handler(
+    event,
+    vk_api,
+    google_project_id,
+    language_code='ru_RU',
+    logger=None
+):
+    try:
+        reply = get_response_to_message(
+            google_project_id,
+            event.user_id,
+            event.text,
+            allow_fallback=False
         )
+
+        if reply:
+            vk_api.messages.send(
+                user_id=event.user_id,
+                message=reply,
+                random_id=random.randint(1, 1000)
+            )
+    except Exception as e:
+        if logger:
+            logger.exception(e)
 
 
 def main():
+    vk_admin_id = env.str('VK_ADMIN_ID')
     google_project_id = env.str('GOOGLE_PROJECT_ID')
     vk_group_token = env.str('VK_GROUP_TOKEN')
+
     vk_session = vk.VkApi(token=vk_group_token)
     vk_api = vk_session.get_api()
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(VkLogsHandler(vk_api, vk_admin_id))
+
     longpoll = VkLongPoll(vk_session)
+    logger.info('VK-Бот запущен')
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
