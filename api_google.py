de42e36473e53bf9b568219b964740118e92adfc
Alexey Pesotsky <pesthedog@mail.ru>
2022-06-24 14:58:36 +0400

    Move create_intents in api_google

--- api_google.py
+++ api_google.py
@@ -29,3 +29,36 @@ def get_response_to_message(
         return None
 
     return response.query_result.fulfillment_text
+
+
+def create_intent(
+    project_id,
+    display_name,
+    training_phrases_parts,
+    message_texts
+):
+    """Create an intent of the given intent type."""
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part
+        )
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message]
+    )
+
+    intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
--- create_intents.py
+++ create_intents.py
@@ -2,43 +2,11 @@ import argparse
 import json
 
 from environs import Env
-from google.cloud import dialogflow
+
+from api_google import create_intent
 
 env = Env()
 env.read_env()
-
-
-def create_intent(
-    project_id,
-    display_name,
-    training_phrases_parts,
-    message_texts
-):
-    """Create an intent of the given intent type."""
-
-    intents_client = dialogflow.IntentsClient()
-
-    parent = dialogflow.AgentsClient.agent_path(project_id)
-    training_phrases = []
-    for training_phrases_part in training_phrases_parts:
-        part = dialogflow.Intent.TrainingPhrase.Part(
-            text=training_phrases_part
-        )
-        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
-        training_phrases.append(training_phrase)
-
-    text = dialogflow.Intent.Message.Text(text=message_texts)
-    message = dialogflow.Intent.Message(text=text)
-
-    intent = dialogflow.Intent(
-        display_name=display_name,
-        training_phrases=training_phrases,
-        messages=[message]
-    )
-
-    intents_client.create_intent(
-        request={"parent": parent, "intent": intent}
-    )
 
 
 def main():
